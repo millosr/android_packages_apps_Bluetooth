ed6529284ba4afb71c6a54531955a3566d481b27
Christopher Tate <ctate@google.com>
2017-02-09 11:47:55 -0800

    Send Bt connection-state-changed broadcasts to manifest receivers
    Connection-state notifications are intentionally allowed to launch apps
    that have subscribed to them.
    
    Bug 35207510
    
    Change-Id: I8d2c9ff53f7338e3f7f43cdde9419cc29aaaab22

--- src/com/android/bluetooth/a2dp/A2dpStateMachine.java
+++ src/com/android/bluetooth/a2dp/A2dpStateMachine.java
@@ -773,7 +773,8 @@ final class A2dpStateMachine extends StateMachine {
             intent.putExtra(BluetoothProfile.EXTRA_PREVIOUS_STATE, prevState);
             intent.putExtra(BluetoothProfile.EXTRA_STATE, state);
             intent.putExtra(BluetoothDevice.EXTRA_DEVICE, device);
-            intent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT);
+            intent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT
+                    | Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
             mContext.sendBroadcast(intent, ProfileService.BLUETOOTH_PERM);
             log("Connection state " + device + ": " + prevState + "->" + state);
             mService.notifyProfileConnectionStateChanged(device, BluetoothProfile.A2DP, state, prevState);
